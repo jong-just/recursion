e37a44f3f78a7eb432b4aa0e7a005295a343c7f2
justin h <joosteen96@gmail.com>
2023-01-04 14:08:35 -0800

    Complete merge sort
    Bug I was having before was that I was not using the return
    from previous recursion. When I tried, there were errors
    of using methods on nil. But I just made the base case
    return the array itself, which fixed the bug.

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,20 +1,15 @@
-# base case is length of array < 2
-# merge_sort
-# if n < 2
-#   return
-# 
 rand_array = [73, 91, 71, 27, 85, 96, 5, 40]
 
-temp_array = [73, 91, 71, 27, 85, 96, 5, 40, 17, 44, 47,
+big_array = [73, 91, 71, 27, 85, 96, 5, 40, 17, 44, 47,
               89, 63, 14, 77, 59, 51, 26, 45, 50, 9, 7, 
               52, 33, 72, 38, 94, 74, 95, 29, 20, 4, 2, 
               54, 67, 75, 53, 69, 98, 62, 60, 64]
 
 module MySorts
   def merge_sort(array)
+    # base case
     if array.size < 2
-      # p array
-      return
+      return array
     end
 
     # halving the arrays
@@ -23,32 +18,33 @@ module MySorts
     temp_array_right = array[(half_point)..array.size-1]
 
     # recursive call
-    p merge_sort(temp_array_left)
-    p merge_sort(temp_array_right)
+    sorted_array_left = merge_sort(temp_array_left)
+    sorted_array_right = merge_sort(temp_array_right)
     puts "Left: #{temp_array_left}"
     puts "Right: #{temp_array_right}"
 
 
-    # merging?
+    # merging
     i = 0
     j = 0
-    left = temp_array_left.size
-    right = temp_array_right.size
+    left = sorted_array_left.size
+    right = sorted_array_right.size
     temp_array = []
-
+    
+    # merge logic
     until j == right && i == left do
-      puts "Comparing || Left: #{temp_array_left[i]} Right: #{temp_array_right[j]}"
-      if temp_array_left[i] == nil
-        temp_array << temp_array_right[j]
+      puts "Comparing || Left: #{sorted_array_left[i]} Right: #{sorted_array_right[j]}"
+      if sorted_array_left[i] == nil
+        temp_array << sorted_array_right[j]
         j+=1
-      elsif temp_array_right[j] == nil
-        temp_array << temp_array_left[i]
+      elsif sorted_array_right[j] == nil
+        temp_array << sorted_array_left[i]
         i+=1
-      elsif temp_array_left[i] > temp_array_right[j]
-        temp_array << temp_array_right[j]
+      elsif sorted_array_left[i] > sorted_array_right[j]
+        temp_array << sorted_array_right[j]
         j+=1
-      elsif temp_array_left[i] < temp_array_right[j]
-        temp_array << temp_array_left[i]
+      elsif sorted_array_left[i] < sorted_array_right[j]
+        temp_array << sorted_array_left[i]
         i+=1
       end
       puts "Outputted Array: #{temp_array} i: #{i} / #{left} j: #{j} / #{right}\n \n"
@@ -60,4 +56,4 @@ end
 
 include MySorts
 
-puts "Sorted: #{merge_sort(rand_array)}"
+puts "Sorted: #{merge_sort(big_array)}"
